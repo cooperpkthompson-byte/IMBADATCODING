1d93949e2365d6bfe130deff4b17fe02195d058c
COOPER <cooperpkthompson@gmail.com>
2025-09-17 11:46:46 -0400

    etshst

--- etshst/playerscene.gd
+++ etshst/playerscene.gd
@@ -3,16 +3,17 @@ extends CharacterBody2D
 
 const SPEED = 300.0
 
-
+var rotation_direction = 0
 func _physics_process(delta: float) -> void:
 
-
+	rotation_direction = Input.get_axis("left","right")
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("ui_down", "ui_up")
 	if direction:
-		velocity.x = direction * SPEED
-	else:
+		velocity = direction * SPEED * transform.x
+	else: 
 		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.y = move_toward(velocity.y, 0, SPEED)
 
 	move_and_slide()
